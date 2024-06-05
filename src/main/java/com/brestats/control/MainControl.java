9e878267124c7ebccb235d8797d799de65a1300a
Nathan <nathanalex53950@gmail.com>
2024-06-05 17:17:38 +0200

    fix: MainControl.java

--- src/main/java/com/brestats/control/MainControl.java
+++ src/main/java/com/brestats/control/MainControl.java
@@ -41,6 +41,7 @@ public class MainControl {
 
     private DBCommune dbCom;
     private WebEngine engine; 
+    private Commune selectedCity = null;
 
     public MainControl() {
         this.dbCom = DAO.DB_COM;
@@ -66,6 +67,7 @@ public class MainControl {
             public void changed(ObservableValue<? extends Number> obs, Number oldValue, Number newValue) {
                 Commune nearestCity = findNearestCity(coords.getLatitude(), coords.getLongitude());
                 searchBar.setText(nearestCity.getNomCommune());
+                selectedCity = nearestCity;
             }
 
             private Commune findNearestCity(double lat, double lng) {
@@ -88,24 +90,19 @@ public class MainControl {
 
     @FXML
     public void handleSearch(Event ev) {
-        String searchQuery = this.searchBar.getText();
-        
-        try {
-            ArrayList<Commune> res = this.dbCom.selectQuery("SELECT * FROM commune WHERE nomCommune = '" + searchQuery + "' OR idCommune = '" + searchQuery + "';");
-            System.out.println(res);
-
-            Parent results = FXMLLoader.load(getClass().getResource("/com/brestats/pages/Results.fxml"));
-            Stage stage= (Stage) ((Node) ev.getSource ()).getScene ().getWindow ();
-            stage.setScene(new Scene(results));
-            System.out.println("change");
-        } catch(IOException ex1) {
-            System.out.println("Cannot change scene");
-            ex1.printStackTrace();
-        } catch(SQLException ex2) {
-            Alert errorAlert = new Alert(AlertType.ERROR, "There is an error in your research. Please check what you typed.", ButtonType.OK);
+        if(this.selectedCity != null) {
+            try {
+                Parent results = FXMLLoader.load(getClass().getResource("/com/brestats/pages/Results.fxml"));
+                Stage stage= (Stage) ((Node) ev.getSource ()).getScene ().getWindow ();
+                stage.setScene(new Scene(results));
+                System.out.println("change");
+            } catch(IOException ex) {
+                System.out.println("Cannot change scene");
+                ex.printStackTrace();
+            }
+        } else {
+            Alert errorAlert = new Alert(AlertType.ERROR, "Please enter the name of a city.", ButtonType.OK);
             errorAlert.show();
-            
-            ex2.printStackTrace();
         }
     }
 
@@ -128,7 +125,8 @@ public class MainControl {
                                 longitudes.add(commune.getLongitude());
                             }
                             engine.executeScript("setMarkers(" + transformToJavascriptArray(latitudes) + "," + transformToJavascriptArray(longitudes) + ")");
-                
+                            
+                            selectedCity = res.get(0);
                         } catch(SQLException ex) {
                             System.out.println("Unexpected exception with query : " + searchQuery);
                         }
